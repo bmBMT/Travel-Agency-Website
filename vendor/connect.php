b277bc52e96e684985906b681b23a5549ca53b95
Alexandr Samarin <alexandr.samarin.04@mail.ru>
2023-02-12 21:26:42 +0300

    connect bd change

--- vendor/connect.php
+++ vendor/connect.php
@@ -1,6 +1,14 @@
 <?php
-    $connect = mysqli_connect('localhost','root','','project_bd');
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $db_name = 'project_bd';
+
+    $connect = mysqli_connect($host,$user,$password,$db_name);
 
     if (!$connect) {
         die('Error connect to DataBase');
     }
+
+    // Change character set to utf8
+    mysqli_set_charset($connect, "utf8");
